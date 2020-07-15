e5d72ea3c37ac539231d4924c6e9405efe41f349
karon78 <karon78@gmail.com>
2020-07-15 09:10:31 +0300

    added class User and Student

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 "use strict";
+
+class User {
+    constructor(name, surname, age, email) {
+        this._name = name;
+        this._surname = surname;
+        this._age = age;
+        this._email = email;
+    }
+
+
+    getFullName() {
+        return `${this._name} ${this._surname}`
+    }
+}
+
+class Student extends User {
+    constructor(name, surname, year) {
+        super(name, surname);
+        this.year = year;
+    }
+
+    set year(v) {
+        if (typeof v !== 'number') {
+            throw new TypeError('param year mast be a number');
+        }
+        if (v < 2014) {
+            throw new RangeError('param year mast be bigger 2014');
+        }
+        this._year = v;
+    }
+
+    get year() {
+        return this._year;
+    }
+
+    getCourse() {
+        const date = new Date;
+        return date.getFullYear() - this.year + 1;
+
+    }
+}
+
+const student1 = new Student('Sasha', 'Ivanov', 2016)
+console.log(student1.getFullName(), student1.getCourse());
